fe151234ea15d36fe9a2404cd2a5e6834ae7cafa
123tan456 <50978810+123tan456@users.noreply.github.com>
2019-05-24 14:21:45 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,9 +1,26 @@
 #include<stdio.h>
+
+
+int input(int *a,int *b)
+{
+printf("enter the numbers");
+scanf("%d%d",a,b);
+}
+int sum(int *a,int *b)
+{
+ return (*a+*b);
+}
+void output(int s)
+{
+printf("sum=%d",s);
+}
+
 int main()
 {
-  int a,b,sum;
-  printf("Enter the numbers\n");
-  scanf("%d%d",&a,&b);
-  sum=a+b;
-  printf("the sum is %d",sum);
+int a,b,s;
+input(&a,&b);
+s=sum(&a,&b);
+output(s);
 }
+
+
