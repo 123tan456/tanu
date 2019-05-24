fe5ace4a558a67168f368b6ac4b5043ee188545b
123tan456 <50978810+123tan456@users.noreply.github.com>
2019-05-24 15:30:16 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int input(int a[],int *n)
+{
+printf("Enter the size of the array A\n");
+scanf("%d",n);
+printf("enter the array elements\n");
+ for(int i=0;i<*n;i++)
+ {
+  scanf("%d",&a[i]);
+ }
+}
+int add(int a[],int n)
+{
+int sum=0;
+for(int i=0;i<n;i++)
+{
+ sum=sum+a[i];
+}
+return sum;
+}
+int output(int sum)
+{
+printf("the sum of the array elements is %d",sum);
+}
+ int main()
+{
+int a[100],n,sum;
+input(a,&n);
+sum=add(a,n);
+output(sum);
+}
+
